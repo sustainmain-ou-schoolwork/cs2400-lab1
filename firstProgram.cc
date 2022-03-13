f4ef9f39a1c2350d6d7cd96f938a795b5299b21a
Josh Marusek <jm553020@ohio.edu>
2022-03-13 10:36:08 -0400

    download completed lab files

---
+++ firstProgram.cc
@@ -0,0 +1,19 @@
+//*****************************************************************************
+//
+//   program:  firstProgram.cc
+// 
+//   Name:     Josh Marusek
+//   Date:     2021-08-27
+//   
+//   Description: This program prints a greeting.
+// 
+//*****************************************************************************
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello, World!\n";
+    return 0;
+}
